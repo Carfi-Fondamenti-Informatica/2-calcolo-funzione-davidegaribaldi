10dfceb2d63971d21b50b8b8b56446a33f1fd361
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-03-31 17:34:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
 float a=0,b=0,x=0,y=0,z=0;
 cin>>a>>b>>x>>y;
-if(x<0 && y>0)
+if(x<0&&y>0)
 {
     z=(a*x)-(b*y);
 }
-else if(x>=0 && y<=0)
+else if(x>=0&&y<=0)
 {
     z=(a*x*x)-(b*y);
 }
